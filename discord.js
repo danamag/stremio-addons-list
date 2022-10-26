87af2a21c8d19314736b9d140c1bff3545a085b3
Stripes <46821970+danamag@users.noreply.github.com>
2022-10-27 00:07:28 +0300

    Create discord.js

---
+++ discord.js
@@ -0,0 +1,50 @@
+const discordGreeting = () => {
+  const greetings = ["Hey", "Sup", "Yo", "Knock knock", "Attention", "Yesss", "Oh yeah", "Awesome", "Finally"]
+  return greetings[Math.floor(Math.random()*greetings.length)]
+}
+
+const sendDiscordMessage = (addons) => {
+  const payload = {
+      content: `${discordGreeing()}, ${addons.length > 1 ? addons.length+' new addons' : 'a new addon'} appeared in the catalog!`,
+      embeds: []
+  }
+  addons.forEach(addon => {
+    const manifest = addon.manifest
+    const addonUrl = `https://${config['netlify-domain']}/${slug(addon.name)}.html`
+    const embed = {
+        title: manifest.name,
+        url: addonUrl,
+        thumbnail: {
+            url: manifest.logo,
+        },
+        description: manifest.description,
+        color: 9001641,
+        provider: {
+            name: 'Install',
+            url: addonUrl,
+        }
+    }
+    const labels = addon.proposedLabels.length ? addon.proposedLabels : (addon.labels || []).map(label => label.name).filter(el => !! el)
+    if ((labels || []).length)
+      embed.fields = [
+        {
+          name: 'Type',
+          value: labels.map(label => label.toLowerCase()).join(', '),
+        },
+      ]
+    payload.embeds.push(embed)
+  })
+  const needleOpts = Object.assign({}, config.needle)
+  needleOpts.json = true
+  console.log(`sending discord message about ${addons.length} new addons`)
+  needle.post(process.env.DISCORD_WEBHOOK, payload, needleOpts, (err, resp, body) => {
+    if (!err)
+      console.log(`sent discord message about ${addons.length} new addons`)
+    else {
+      console.log(`error when sending discord message about ${addons.length} new addons`)
+      console.error(err)
+    }
+  })
+}
+
+module.exports = sendDiscordMessage
