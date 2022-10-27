01f1ca63221060249b2da44b49488fb0e1361481
Stripes <46821970+danamag@users.noreply.github.com>
2022-10-27 11:04:11 +0300

    Update build.js

--- build.js
+++ build.js
@@ -6,6 +6,7 @@ const slug = require('slug')
 const config = require('./config')
 const graphql = require('./graphql')
 const getCached = require('./cache')
+const processHtml = require('./html')
 
 getCached().then(cached => {
   if (cached.time && cached.time > Date.now() - config['prefer-cached-for']) {
@@ -110,79 +111,11 @@ getCached().then(cached => {
       }
     })
 
-    const listAddonHtml = fs.readFileSync('./template/home/list-addon.html').toString()
-    const addonPageHeader = fs.readFileSync('./template/addon/header.html').toString()
-    const addonPageFooter = fs.readFileSync('./template/addon/footer.html').toString()
-    const addonPageContent = fs.readFileSync('./template/addon/content.html').toString()
-
     const dir = './out';
 
-    if (!fs.existsSync(dir))
-        fs.mkdirSync(dir)
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir)
 
     const listHtml = []
-
-    const addDataForAddon = (listAddonHtml, body, task, labelsAreLinks) => {
-      let addonHtml = listAddonHtml
-      if (!body && !task) {
-        // only the home page can be in this scenario (header / footer)
-        addonHtml = addonHtml.split('{home-keywords}').join(config['meta-keywords'])
-        addonHtml = addonHtml.split('{home-page-title}').join(config['page-title'])
-        addonHtml = addonHtml.split('{home-meta-title}').join(config['meta-title'])
-        addonHtml = addonHtml.split('{home-netlify-domain}').join(config['netlify-domain'])
-        addonHtml = addonHtml.split('{home-favicon}').join(config['meta-favicon'])
-        addonHtml = addonHtml.split('{home-description}').join(config['meta-description'])
-        addonHtml = addonHtml.split('{repo-name}').join(config.author+'/'+config.repository)
-        return addonHtml
-      }
-      // we replace netlify domain here again for the addon page header template
-      addonHtml = addonHtml.split('{home-netlify-domain}').join(config['netlify-domain'])
-      addonHtml = addonHtml.split('{addon-page-title-append}').join(config['meta-addon-title-append'])
-      let labelsHtml = task.labels.map(el => el.name.split(' ').join('-')).join(' ')
-      if (labelsHtml)
-        labelsHtml = ' ' + labelsHtml
-      addonHtml = addonHtml.replace('{labels}', labelsHtml)
-      addonHtml = addonHtml.replace('{addon-id}', body.id)
-      addonHtml = addonHtml.replace('{addon-version}', body.version)
-      addonHtml = addonHtml.split('{addon-title}').join(body.name)
-      if (body.description)
-        addonHtml = addonHtml.split('{addon-description}').join(body.description)
-      else
-        addonHtml = addonHtml.split('{addon-description}').join('')
-      const lowerCaseName = body.name.toLowerCase()
-      addonHtml = addonHtml.replace('{addon-keywords}', [lowerCaseName, lowerCaseName + ' down', lowerCaseName + ' down or just me', lowerCaseName + ' site down', lowerCaseName + ' not working', lowerCaseName + ' not found', 'stremio addons', 'addons list'])
-      addonHtml = addonHtml.split('{addon-logo}').join(body.logo || body.icon)
-      addonHtml = addonHtml.replace('{addon-types}', task.labels.map(el => `<${labelsAreLinks ? 'a href="https://' + config['netlify-domain'] + '/' + (el.name === '<ion-icon class="back-arrow" name="arrow-back-outline"></ion-icon> all addons' ? '' : '?label=' + el.name.split(' ').join('-')) + '"' : 'span'} class="label label-addon-page" style="background-color: #${el.color}">${el.name}</${labelsAreLinks ? 'a' : 'span'}>`).join(''))
-      addonHtml = addonHtml.replace('{addon-types-small}', task.labels.map(el => `<${labelsAreLinks ? 'a href="https://' + config['netlify-domain'] + '/' + (el.name === '<ion-icon class="back-arrow" name="arrow-back-outline"></ion-icon> all addons' ? '' : '?label=' + el.name.split(' ').join('-')) + '"' : 'span'} class="label label-small" style="background-color: #${el.color}">${el.name}</${labelsAreLinks ? 'a' : 'span'}>`).join(''))
-      addonHtml = addonHtml.replace('{addon-score}', task.score)
-      addonHtml = addonHtml.replace('{addon-ups}', task.ups)
-      addonHtml = addonHtml.replace('{addon-downs}', task.downs)
-      if (!task.ups && !task.downs)
-        addonHtml = addonHtml.replace('{addons-score-faded}', ' addon-score-faded')
-      else
-        addonHtml = addonHtml.replace('{addons-score-faded}', '')
-      if (!(body.behaviorHints || {}).configurationRequired)
-        addonHtml = addonHtml.replace('{install-button}', '<a class="addon-button install-button" href="'+task.url.replace('https://','stremio://')+'">Install</a> <a class="addon-button copy-link-button" href="#" onClick="copyLink(event, \''+task.url+'\')">Copy Link</a>')
-      else
-        addonHtml = addonHtml.replace('{install-button}', '')
-      if ((body.behaviorHints || {}).configurable)
-        addonHtml = addonHtml.replace('{configure-button}', '<a class="addon-button configure-button" href="'+task.url.replace('/manifest.json','/configure')+'" target="_blank">Configure</a>')
-      else
-        addonHtml = addonHtml.replace('{configure-button}', '')
-      if (task.commentCount)
-        addonHtml = addonHtml.replace('{comments-button}', `<a href="${slug(body.name)}.html" class="addon-button last-addon-button"><ion-icon name="chatbubbles" class="gray-icon"></ion-icon> ${task.commentCount}</a>`)
-      else
-        addonHtml = addonHtml.replace('{comments-button}', '')
-      addonHtml = addonHtml.split('{addon-page}').join(`${slug(body.name)}.html`)
-      addonHtml = addonHtml.split('{issue-url}').join(task.issueUrl)
-      addonHtml = addonHtml.split('{repo-name}').join(config.author+'/'+config.repository)
-      if (task.language && task.language !== 'Multilingual')
-        addonHtml = addonHtml.split('{addon-language}').join(`<div class="addon-language">${task.language} Content</div>`)
-      else
-        addonHtml = addonHtml.split('{addon-language}').join('')
-      return addonHtml
-    }
-
     const newAddons = []
 
     const queue = asyncQueue((task, cb) => {
@@ -201,16 +134,62 @@ getCached().then(cached => {
           task.manifest = addonManifest
           newAddons.push(task)
         }
-        const addonHtml = addDataForAddon(listAddonHtml, addonManifest, task)
+        
+        let labelsHtml = task.labels.map(el => el.name.split(' ').join('-')).join(' ')
+        if (labelsHtml) labelsHtml = ' ' + labelsHtml
+
+        const lowerCaseName = addonManifest.name.toLowerCase()
+        const keywordsForAddonPage = [lowerCaseName, lowerCaseName + ' down', lowerCaseName + ' down or just me', lowerCaseName + ' site down', lowerCaseName + ' not working', lowerCaseName + ' not found', 'stremio addons', 'addons list']
+
+        const installButton = !(addonManifest.behaviorHints || {}).configurationRequire ? '<a class="addon-button install-button" href="'+task.url.replace('https://','stremio://')+'">Install</a> <a class="addon-button copy-link-button" href="#" onClick="copyLink(event, \''+task.url+'\')">Copy Link</a>' : ''
+        const configButton = (addonManifest.behaviorHints || {}).configurable ? '<a class="addon-button configure-button" href="'+task.url.replace('/manifest.json','/configure')+'" target="_blank">Configure</a>' : ''
+        const commentsButton = task.commentCount ? `<a href="${slug(addonManifest.name)}.html" class="addon-button last-addon-button"><ion-icon name="chatbubbles" class="gray-icon"></ion-icon> ${task.commentCount}</a>` : ''
+        const language = task.language && task.language !== 'Multilingual' ? `<div class="addon-language">${task.language} Content</div>` : ''
+        const addonsScoreFaded = !task.ups && !task.downs ? ' addon-score-faded' : ''
+
+        const labelsForHomeHeader = task.labels.map(el => `<span class="label label-addon-page" style="background-color: #${el.color}">${el.name}</span>`).join('')
+        const labelsForHomeAddon = task.labels.map(el => `<span class="label label-small" style="background-color: #${el.color}">${el.name}</span>`).join('')
+        const labelsForAddonPage = task.labels.map(el => `<${'a href="https://' + config['netlify-domain'] + '/' + (el.name === '<ion-icon class="back-arrow" name="arrow-back-outline"></ion-icon> all addons' ? '' : '?label=' + el.name.split(' ').join('-')) + '"'} class="label label-addon-page" style="background-color: #${el.color}">${el.name}</a>`).join('')
+        
+        const map = {
+          '{home-netlify-domain}': config['netlify-domain'],
+          '{addon-page-title-append}': config['meta-addon-title-append'],
+          '{labels}': labelsHtml,
+          '{addon-id}': addonManifest.id,
+          '{addon-version}': addonManifest.version,
+          '{addon-title}': addonManifest.name,
+          '{addon-description}': addonManifest.description || '',
+          '{addon-keywords}': keywordsForAddonPage,
+          '{addon-logo}': addonManifest.logo || addonManifest.icon,
+          '{addon-types}': labelsForHomeHeader,
+          '{addon-types-small}': labelsForHomeAddon,
+          '{addon-types-links}': labelsForAddonPage,
+          '{addon-score}': task.score,
+          '{addon-ups}': task.ups,
+          '{addon-downs}': task.downs,
+          '{addons-score-faded}': addonsScoreFaded,
+          '{install-button}': installButton,
+          '{configure-button}': configButton,
+          '{comments-button}': commentsButton,
+          '{addon-page}': `${slug(addonManifest.name)}.html`,
+          '{issue-url}': task.issueUrl,
+          '{issue-number}': task.issueNumber
+          '{repo-name}': config.author+'/'+config.repository,
+          '{addon-language}': language,
+          '{addon-url}': task.url,
+        }
+
+        const addonHtml = processHtml('homePageAddon', map)
+
         task.labels = [{ color: 'A08C80', name: '<ion-icon class="back-arrow" name="arrow-back-outline"></ion-icon> all addons' }].concat(task.labels)
-        let parsedAddonPage = addDataForAddon(addonPageContent, addonManifest, task, true)
-        parsedAddonPage = parsedAddonPage.replace('{addon-list-item}', addonHtml)
-        parsedAddonPage = parsedAddonPage.replace('{issue-number}', task.issueNumber)
-        const parsedAddonHeader = addDataForAddon(addonPageHeader, addonManifest, task)
-        let parsedAddonFooter = addDataForAddon(addonPageFooter, addonManifest, task)
-        parsedAddonFooter = parsedAddonFooter.replace('{addon-url}', task.url)
+        labelsHtml = task.labels.map(el => el.name.split(' ').join('-')).join(' ')
+        if (labelsHtml) labelsHtml = ' ' + labelsHtml
+        map['{labels}'] = labelsHtml
+        
+        const parsedAddonPage = processHtml('addonPage', map)
+
         console.log('creating page for addon: ' + addonManifest.name)
-        fs.writeFileSync(`${dir}/${slug(addonManifest.name)}.html`, parsedAddonHeader+parsedAddonPage+parsedAddonFooter)
+        fs.writeFileSync(`${dir}/${slug(addonManifest.name)}.html`, parsedAddonPage)
         task.labels.shift() // remove "all addons" prefix from labels
         listHtml.push(addonHtml)
         cb()
@@ -245,17 +224,6 @@ getCached().then(cached => {
       })
     }, 1)
 
-    let header = fs.readFileSync('./template/home/header.html').toString()
-    // move "misc" label to end of list
-    const miscLabelIndex = all_labels.findIndex(label => label.name === 'misc')
-    if (miscLabelIndex > -1)
-      all_labels.push(all_labels.splice(miscLabelIndex, 1)[0])
-    header = header.replace('{labels-list}', all_labels.map((el, ij) => `<span class="label${!ij ? ' selected' : ''}" style="background-color: #${el.color}">${el.name}</span>`).join(''))
-    header = addDataForAddon(header)
-    const footer = fs.readFileSync('./template/home/footer.html').toString()
-
-    const parsedFooter = addDataForAddon(footer)
-
     queue.drain = () => {
       if (process.env.DISCORD_WEBHOOK && newAddons.length)
         sendDiscordMessage(newAddons)
@@ -271,7 +239,23 @@ getCached().then(cached => {
       console.log('creating addons catalog json file')
       fs.writeFileSync(`${dir}/catalog.json`, JSON.stringify(addons_collection))
       console.log('creating home page')
-      fs.writeFileSync(`${dir}/index.html`, header+listHtml.join('')+parsedFooter)
+      // move "misc" label to end of list
+      const miscLabelIndex = all_labels.findIndex(label => label.name === 'misc')
+      if (miscLabelIndex > -1)
+        all_labels.push(all_labels.splice(miscLabelIndex, 1)[0])
+      const map = {
+        '{home-keywords}': config['meta-keywords'],
+        '{home-page-title}': config['page-title'],
+        '{home-meta-title}': config['meta-title'],
+        '{home-netlify-domain}': config['netlify-domain'],
+        '{home-favicon}': config['meta-favicon'],
+        '{home-description}': config['meta-description'],
+        '{repo-name}': config.author+'/'+config.repository,
+        '{labels-list}': all_labels.map((el, ij) => `<span class="label${!ij ? ' selected' : ''}" style="background-color: #${el.color}">${el.name}</span>`).join(''),
+        '{addons-list}': listHtml.join(''),
+      }
+      const homePage = processHtml('homePage', map)
+      fs.writeFileSync(`${dir}/index.html`, homePage)
       if (!cached.prefer) {
         console.log('saving timestamp of last update to json')
         fs.writeFileSync(`${dir}/lastUpdate.json`, JSON.stringify({ time: Date.now() }))
